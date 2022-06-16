08ee314b1768fd427b6dd5b5a9a08a9d5e7271db
jueon.park <bluegbgb@gmail.com>
2022-06-16 21:14:23 +0900

    edit api interface - 계좌 관련 정보를 따로 설정하도록 분리함

--- pykis/public_api.py
+++ pykis/public_api.py
@@ -12,10 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 class Api:
-    def __init__(self, key_info) -> None:
+    def __init__(self, key_info, account_info = None) -> None:
         """
-        key_info: API 사용을 위한 인증키 정보
+        key_info: API 사용을 위한 인증키 정보. appkey, appsecret
+        account_info: 사용할 계좌 정보. account_code, account_product_code 
+        """
+        return
+
+    def set_account(self, account_info):
+        """
+        사용할 계좌 정보 설정.
+        account_info: 
         """
         return
 
@@ -30,41 +39,37 @@ class Api:
     # 시세 조회------------
 
     # 잔고 조회------------
-    def get_kr_buyable_cash(self, account_code: str):
+    def get_kr_buyable_cash(self):
         """
         구매 가능 현금(원화) 조회
-        account_code: 조회하려는 계좌 정보
         return: 해당 계좌의 구매 가능한 현금(원화)
         """
         return
 
-    def get_kr_stock_balance(self, account_code: str):
+    def get_kr_stock_balance(self):
         """
         국내 주식 잔고 조회
-        account_code: 조회하려는 계좌 정보
         return: 국내 주식 잔고 정보를 DataFrame으로 반환
         """
         return
     # 잔고 조회------------
 
     # 매매-----------------
-    def buy_kr_stock(self, ticker: str, order_amount: int, price: int, account_code: str):
+    def buy_kr_stock(self, ticker: str, order_amount: int, price: int):
         """
         국내 주식 매수(현금)
         ticker: 종목코드
         order_amount: 주문 수량
         price: 주문 가격
-        account_code: 매매를 진행하려는 계좌 정보
         """
         return
 
-    def sell_kr_stock(self, ticker: str, order_amount: int, price: int, account_code: str):
+    def sell_kr_stock(self, ticker: str, order_amount: int, price: int):
         """
         국내 주식 매매(현금)
         ticker: 종목코드
         order_amount: 주문 수량
         price: 주문 가격
-        account_code: 매매를 진행하려는 계좌 정보
         """
         return
     # 매매-----------------
