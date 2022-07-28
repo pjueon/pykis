b46377a221e595705af700f8fe636b7d8d8a7fa8
jueon.park <bluegbgb@gmail.com>
2022-07-28 22:21:28 +0900

    MarketCodeMap에 누락된 뉴욕거래소 코드 추가

--- src/pykis/market_code_map.py
+++ src/pykis/market_code_map.py
@@ -27,6 +27,7 @@ class MarketCodeMap:
         self.map_3_to_4 = {
             "NAS": "NASD",
             "AMS": "AMEX",
+            "NYSE": "NYS",
             "TSE": "TKSE",
             "SHS": "SHAA",
             "SZS": "SZAA",
